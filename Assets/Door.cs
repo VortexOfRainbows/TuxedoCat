6e5ce4ca0cdedf072cacffe22eb989e6faa3c9c9
Vortex <vortexsnail12@gmail.com>
2025-04-28 02:49:37 -0700

    WIP level design changes, climbable tiles, door

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -5,17 +5,38 @@ public class Door : MonoBehaviour
     public GameObject Gate;
     public bool Open = false;
     public DoorButton[] Button;
+    public SpriteRenderer[] Light;
     private void Start()
     {
 
     }
     private void FixedUpdate()
     {
-        if (CheckOpen())
+        Open = CheckOpen();
+        if(Open)
         {
-            Open = true;
-            Gate.SetActive(false);
+            Gate.transform.localPosition = Gate.transform.localPosition.Lerp(new Vector3(0, 0.5f, 0), 0.025f);
+            if(Gate.transform.localPosition.y < 0.5f)
+                Gate.transform.localPosition += new Vector3(0, 0.01f);
         }
+        else {
+            Gate.transform.localPosition = Gate.transform.localPosition.Lerp(new Vector3(0, -1.5f, 0), 0.025f);
+            if(Gate.transform.localPosition.y > -1.5f)
+                Gate.transform.localPosition -= new Vector3(0, 0.01f);
+        }
+        for (int i = 0; i < Button.Length; ++i)
+            if (Button[i].Toggled)
+                TurnOnLight(i);
+            else
+                TurnOffLight(i);
+    }
+    private void TurnOnLight(int i)
+    {
+        Light[i].color = Color.Lerp(Light[i].color, Color.white * 1.5f, 0.1f);
+    }
+    private void TurnOffLight(int i)
+    {
+        Light[i].color = Color.Lerp(Light[i].color, Color.black, 0.1f);
     }
     private bool CheckOpen()
     {
--- Assets/DoorButton.cs
+++ Assets/DoorButton.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     public Door Parent;
+    public GameObject PressurePlate;
     public bool ResetsWhenNotActive = false;
     public bool Toggled = false;
     public bool DoorOpened => Parent.Open;
+    public void FixedUpdate()
+    {
+        if(Toggled)
+        {
+            PressurePlate.transform.localPosition = PressurePlate.transform.localPosition.Lerp(new Vector3(0, -0.125f, 0), 0.14f);
+        }
+        else
+        {
+            PressurePlate.transform.localPosition = PressurePlate.transform.localPosition.Lerp(Vector3.zero, 0.14f);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") || collision.CompareTag("Spider"))
             Toggled = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        OnTriggerEnter2D(collision);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
