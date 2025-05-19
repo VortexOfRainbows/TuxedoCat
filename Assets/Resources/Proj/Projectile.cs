7524b509e6489e025e60b5add65fc166feeeb217
Vortex <vortexsnail12@gmail.com>
2025-05-19 03:50:59 -0700

    Update Projectile.cs

--- Assets/Resources/Proj/Projectile.cs
+++ Assets/Resources/Proj/Projectile.cs
@@ -92,7 +92,7 @@ public class Laser : Projectile
         cmp.spriteRenderer.material = Resources.Load<Material>("Additive");
         Friendly = true;
         Hostile = false;
-        AudioManager.PlaySound(SoundID.Laser, transform.position, 1, Utils.RandFloat(1.2f, 1.4f));
+        AudioManager.PlaySound(SoundID.Laser, transform.position, 0.6f, Utils.RandFloat(1.2f, 1.4f));
     }
     public void SpawnParticles()
     {
